758c2e6bcc93df8859ffa4af4630689fa0e66d7a
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-17 19:48:30 +0100

    FEAT: Added chefs to seed file

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
     collection do
       get :top
     end
+
+    member do
+      get :chef
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,15 @@
 puts "Cleaning database..."
 Restaurant.destroy_all
 
+
 puts "Creating restaurants..."
-dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", stars: 5 }
-pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", stars: 4 }
 
-[ dishoom, pizza_east ].each do |attributes|
-  restaurant = Restaurant.create!(attributes)
-  puts "Created #{restaurant.name}"
-end
-
-20.times do
+25.times do
   attributes =  { name: Faker::Restaurant.name , address: Faker::Address.full_address, stars: rand(1..5) }
   restaurant = Restaurant.create!(attributes)
   puts "Created #{restaurant.name}"
+  chef = Chef.create!( name: Faker::Artist.name, restaurant: restaurant)
+  puts "Chef #{chef.name} was added to #{restaurant.name}"
 end
 
 puts "Finished!"
